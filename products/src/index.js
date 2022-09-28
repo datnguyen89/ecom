d600dd4065d486243b8f2c0ff49e69a8c5f146a6
Dat Nguyen <datnt@irismedia.vn>
2022-09-28 09:46:16 +0700

    .

--- products/src/index.js
+++ products/src/index.js
@@ -6,5 +6,4 @@ for (let i = 0; i < 5; i++) {
     const name = faker.commerce.productName();
     products += `<div>${name}</div>`
 }
-console.log(products)
 document.querySelector('#dev-products').innerHTML = products
